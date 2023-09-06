0cd75d7f8729eb25152e841773cb14023f2a699e
Sippakorn Bunyu <charliez.2xxx@gmail.com>
2023-09-06 23:59:36 +0700

    Added 7.1

---
+++ ปฏิบัติการ/Module Class Method/import_my_avg.py
@@ -0,0 +1,6 @@
+import my_avg
+
+l = [3,7,4,5,9,2,1]
+t = (1,9,2,8)
+
+print('The average of list \'l\' is {:.2f}\nThe average of tuple \'t\' is {:.2f}'.format(my_avg.avg(l),my_avg.avg(t)))
---
+++ ปฏิบัติการ/Module Class Method/my_avg.py
@@ -0,0 +1,3 @@
+def avg(x):
+    n = sum(x)/len(x)
+    return n
